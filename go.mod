c93030d8a2fb52fb32c7fed2188ee4dca39582b5
kotekaman <kotekaman@kotekamans-MacBook-Pro.local>
2021-07-19 10:42:53 +0700

    add logging

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.16
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
+	github.com/sirupsen/logrus v1.4.2
 	go.mongodb.org/mongo-driver v1.6.0
 	google.golang.org/grpc v1.39.0
 	google.golang.org/protobuf v1.27.1
--- server/server.go
+++ server/server.go
@@ -5,17 +5,53 @@ import (
 	"k-grpc/service"
 	"log"
 
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+
+	"github.com/sirupsen/logrus"
+
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
 
 var (
 	talentService entitypb.TalentServiceServer = service.NewTalentService()
+
+	logrusLogger = logrus.New()
+	customFunc   = func(code codes.Code) logrus.Level {
+		if code == codes.OK {
+			return logrus.InfoLevel
+		}
+		return logrus.ErrorLevel
+	}
 )
 
 func GrpcServer() *grpc.Server {
 	log.Println("Server Starting")
+	logrusEntry := logrus.NewEntry(logrusLogger)
 
-	grpcServer := grpc.NewServer()
+	// Shared options for the logger, with a custom gRPC code to log level function.
+	opts := []grpc_logrus.Option{
+		grpc_logrus.WithLevels(customFunc),
+	}
+
+	// Make sure that log statements internal to gRPC library are logged using the logrus Logger as well.
+	grpc_logrus.ReplaceGrpcLogger(logrusEntry)
+
+	// Create a server, make sure we put the grpc_ctxtags context before everything else.
+	grpcServer := grpc.NewServer(
+		grpc_middleware.WithUnaryServerChain(
+			grpc_ctxtags.UnaryServerInterceptor(grpc_ctxtags.WithFieldExtractor(grpc_ctxtags.CodeGenRequestFieldExtractor)),
+			grpc_logrus.UnaryServerInterceptor(logrusEntry, opts...),
+		),
+		grpc_middleware.WithStreamServerChain(
+			grpc_ctxtags.StreamServerInterceptor(grpc_ctxtags.WithFieldExtractor(grpc_ctxtags.CodeGenRequestFieldExtractor)),
+			grpc_logrus.StreamServerInterceptor(logrusEntry, opts...),
+		),
+	)
+
+	//grpcServer := grpc.NewServer()
 	entitypb.RegisterTalentServiceServer(grpcServer, talentService)
 
 	return grpcServer
